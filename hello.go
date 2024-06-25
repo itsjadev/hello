db6fa121dfd18960bf3e71a0769f8c4b94ad5862
Jade Vasconcellos <jdvscnclls@gmail.com>
2024-06-25 18:30:36 -0300

    funções e pacotes os

--- hello.go
+++ hello.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "Marcos"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do Programa")
-	var comando int
-
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
 
 	//	if comando == 1 {
 	//		fmt.Println("Monitorando..")
@@ -35,7 +27,30 @@ func main() {
 		fmt.Println("Exibindo Logs")
 	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Marcos"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
