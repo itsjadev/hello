e47772b9e3e31960a4d09fa758d4f673c31969c3
Jade Vasconcellos <jdvscnclls@gmail.com>
2024-06-25 18:10:58 -0300

    aprendendo if e switch

--- hello.go
+++ hello.go
@@ -15,7 +15,27 @@ func main() {
 	fmt.Println("0 - Sair do Programa")
 	var comando int
 
-	fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
 
+	//	if comando == 1 {
+	//		fmt.Println("Monitorando..")
+	//	} else if comando == 2 {
+	//		fmt.Println("Exibindo Logs")
+	//	} else if comando == 0 {
+	//		fmt.Println("Saindo do programa")
+	//	} else {
+	//		fmt.Println("Não conheço este comando")
+	//	}
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando..")
+	case 2:
+		fmt.Println("Exibindo Logs")
+	case 0:
+		fmt.Println("Saindo do programa")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
 }
